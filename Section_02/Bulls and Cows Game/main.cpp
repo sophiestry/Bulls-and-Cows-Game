f98e553165e2a525e4aaea67b798dcd66527b63d
sophiestry <sophie.gmu@gmail.com>
2019-11-30 14:41:15 +0000

    Section_02 23.

--- Section_02/Bulls and Cows Game/main.cpp
+++ Section_02/Bulls and Cows Game/main.cpp
@@ -1,13 +1,42 @@
 #include <iostream>
-
+#include <string>
 using namespace std;
 
 int main()
 {
+	//introduce the game
 	constexpr int WORD_LENGTH = 5;
+
 	cout << "Welcome to Bulls and Cows, a cool child-friendly word game!" << endl;
 	cout << "Can you guess the " << WORD_LENGTH; 
-	cout << " letter isogram I'm thinking of?" << endl;
+	cout << " letter isogram I'm thinking of?" 
+	<< endl;
+
+	//get a guess form the player
+	cout << endl;
+
+	cout << "Take a guess: ";
+	string Guess = "";
+	cin >> Guess;
+	cout << endl;
+
+	//repeat the guess
+	cout << "Your answer was: " << Guess; 
+	cout << "! Nice Try!" 
+		<< endl;
+
+	//ask again
+
+	cout << endl;
+	cout << "Take a guess: ";
+	cin >> Guess;
+	cout << endl;
+
+	//repeat the answer
+	
+	cout << "Your answer was: " << Guess;
+	cout << "! Good one!"
+		<< endl;
 	
 	return 0;
 }
