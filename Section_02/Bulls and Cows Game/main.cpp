33f0bd6e5c2de3db4b98f61328b7cbb6d369cd93
sophiestry <sophie.gmu@gmail.com>
2019-11-30 14:10:00 +0000

    Section_02 22.

--- Section_02/Bulls and Cows Game/main.cpp
+++ Section_02/Bulls and Cows Game/main.cpp
@@ -4,7 +4,10 @@ using namespace std;
 
 int main()
 {
-	cout << "Welcome to Bulls and Cows!" << endl;
+	constexpr int WORD_LENGTH = 5;
+	cout << "Welcome to Bulls and Cows, a cool child-friendly word game!" << endl;
+	cout << "Can you guess the " << WORD_LENGTH; 
+	cout << " letter isogram I'm thinking of?" << endl;
 	
 	return 0;
 }
