2b76390450551eb638d60e69e5c0075fb5c520b2
sophiestry <sophie.gmu@gmail.com>
2019-11-30 15:35:37 +0000

    Section_02 25.

--- Section_02/Bulls and Cows Game/main.cpp
+++ Section_02/Bulls and Cows Game/main.cpp
@@ -2,41 +2,51 @@
 #include <string>
 using namespace std;
 
+void PrintIntro();
+string GetGuessAndPrintBack();
+
+
+//the entry point
 int main()
 {
-	//introduce the game
+	PrintIntro();
+
+	GetGuessAndPrintBack();
+	GetGuessAndPrintBack();
+
+	cout << endl;
+	return 0;
+}
+
+//introduce the game
+void PrintIntro() {
+	
 	constexpr int WORD_LENGTH = 5;
 
 	cout << "Welcome to Bulls and Cows, a cool child-friendly word game!" << endl;
-	cout << "Can you guess the " << WORD_LENGTH; 
-	cout << " letter isogram I'm thinking of?" 
-	<< endl;
+	cout << "Can you guess the " << WORD_LENGTH;
+	cout << " letter isogram I'm thinking of?"
+		<< endl;
+	return;
+}
 
-	//get a guess form the player
+//get a guess form the player
+string GetGuessAndPrintBack() {
+
 	cout << endl;
 
 	cout << "Take a guess: ";
 	string Guess = "";
 	getline(cin, Guess);
-	cout << endl;
+
 
 	//repeat the guess
-	cout << "Your answer was: " << Guess; 
-	cout << "! Nice Try!" 
-		<< endl;
-
-	//ask again
-
-	cout << endl;
-	cout << "Take a guess: ";
-	getline (cin, Guess);
-	cout << endl;
-
-	//repeat the answer
-	
 	cout << "Your answer was: " << Guess;
-	cout << "! Good one!"
+	cout << "! Nice Try!"
 		<< endl;
-	
-	return 0;
+
+	return Guess;
 }
+
+
+	
